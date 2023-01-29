c378d9b9b7531372dcfd76a4066085371d7b406c
MissGin <bj.brewer@att.net>
2023-01-29 05:41:27 -0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-fetch('http://numbersapi.com/random/math?json')
-  .then(response => response.json())
-  .then(data => {console.log(data.text)
-    document.querySelector('#p').innerText = data.text
-  })
+// fetch('http://numbersapi.com/random/math?json')
+//   .then(response => response.json())
+//   .then(data => {console.log(data.text)
+//     document.querySelector('#p').innerText = data.text
+//   })
 
-  fetch('http://numbersapi.com/random/trivia?json')
-  .then(response => response.json())
-  .then(data => {console.log(data.text)
-    document.querySelector('#p2').innerText = data.text
-  })
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bd45900726msh5adf0f5eab08713p173487jsnae5ddad7eca7',
+		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+	}
+};
+
+fetch('https://numbersapi.p.rapidapi.com/random/math?min=10&max=20&fragment=true&json=true', options)
+	.then(response => response.json())
+	.then(data=> { console.log(data)
+        document.querySelector('#p').innerText = data.number + ' is ' + data.text
+    })
+	.catch(err => console.error(err));
+
+fetch('https://numbersapi.p.rapidapi.com/random/trivia?min=10&max=20&fragment=true&json=true', options)
+        .then(response => response.json())
+        .then(data => {console.log(data)
+            document.querySelector('#p2').innerText = data.number + ' is ' + data.text
+          })
+        .catch(err => console.error(err));
+
+//   fetch('http://numbersapi.com/random/trivia?json')
+//   .then(response => response.json())
+//   .then(data => {console.log(data.text)
+//     document.querySelector('#p2').innerText = data.text
+//   })
   
   fetch('https://api.math.tools/numbers/nod')
   .then(response => response.json())
